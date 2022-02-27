8ab05e0306c2413852e81f4d7acaeef87e0b9557
Jaedson Silva <contacanal798@gmail.com>
2022-02-27 00:34:39 -0300

    setup: adicionando configuração de pacote em setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from argeasy import __version__
+
+setup(
+    name='argeasy',
+    version=__version__,
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    packages=['argeasy'],
+    project_urls={
+        'Código fonte': 'https://github.com/jaedsonpys/argeasy'
+    },
+    license='GNU General Public License v3.0'
+)
