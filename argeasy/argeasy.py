c3b7efe1ffe37a6a39facd87be29369a3e27b1eb
Jaedson Silva <contacanal798@gmail.com>
2022-02-28 13:59:49 -0300

    argeasy/argeasy: adicionando teste do ArgEasy em __main__

--- argeasy/argeasy.py
+++ argeasy/argeasy.py
@@ -287,3 +287,56 @@ class ArgEasy(object):
             setattr(namespace, cmd, value)
 
         return namespace
+
+
+if __name__ == '__main__':
+    from random import randint
+
+    colors = {
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'blue': '\033[34m',
+        'close': '\033[m'
+    }
+
+    # test argeasy
+    argeasy = ArgEasy(
+        description='Test of ArgEasy project',
+        version='1.0.0',
+        project_name='ArgEasy'
+    )
+
+    argeasy.add_argument('say', 'Say something')
+    argeasy.add_argument('check_numbers', 'Analyzes whether the numbers are even', action='append')
+    argeasy.add_argument('random', 'Generate random numbers', action='store_true')
+
+    argeasy.add_flag('-c', 'Select color of text (RGB)')
+    argeasy.add_flag('-m', 'Max range numbers')
+
+    args = argeasy.get_args()
+
+    if args.say:
+        if args.c:
+            get_color = colors.get(args.c)
+            close = colors.get('close')
+
+            if not get_color:
+                print('\033[31mChoose a valid color\033[m')
+            else:
+                print(get_color + args.say + close)
+        else:
+            print(args.say)
+    elif args.check_numbers is not None:
+        numbers = args.check_numbers
+        even_numbers = []
+        for n in numbers:
+            if int(n) % 2 == 0:
+                even_numbers.append(n)
+
+        print(f'The even numbers are: {even_numbers}')
+    elif args.random:
+        if not args.m:
+            print(randint(0, 9999))
+        else:
+            limit = int(args.m)
+            print(randint(0, limit))
