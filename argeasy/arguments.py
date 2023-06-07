303916d111085268001a7c5d7a3c4d8e78a47163
Jaedson Silva <jaedson.dev@proton.me>
2023-06-07 15:32:47 -0300

    fix: change class name in repr string

--- argeasy/arguments.py
+++ argeasy/arguments.py
@@ -1,7 +1,7 @@
 class Arguments(object):
     def __repr__(self):
         attr = [i for i in self.__dir__() if not i.startswith('__')]
-        repr = 'Argument('
+        repr = 'Arguments('
 
         for c, a in enumerate(attr):
             value = self.__getattribute__(a)
