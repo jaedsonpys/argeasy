d7c264fc27e83f6435bce9d0a4ec55a28d761a08
Jaedson Silva <jaedson.dev@proton.me>
2023-06-07 17:25:58 -0300

    feat: remove unnecessary exceptions

--- argeasy/exceptions.py
+++ argeasy/exceptions.py
@@ -1,13 +1,3 @@
-class InvalidArgumentUseError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-
-class InvalidFlagUseError(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-
 class InvalidActionError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
